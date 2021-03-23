4a3a7191ad37ebc4a8ba19a2904fa19c9f2be2b5
Matt R <mattreimer1@yahoo.com>
2021-03-22 19:30:02 -0500

    finished assignment #4 Java

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -1,6 +1,12 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.JobRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -8,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
@@ -15,9 +22,19 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    @Autowired
+    EmployerRepository employerRepository;
+
+    @Autowired
+    SkillRepository skillRepository;
+
+    @Autowired
+    JobRepository jobRepository;
+
     @RequestMapping("")
     public String index(Model model) {
 
+        model.addAttribute("jobs",jobRepository.findAll());
         model.addAttribute("title", "My Jobs");
 
         return "index";
@@ -26,6 +43,9 @@ public class HomeController {
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
+
+        model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
@@ -38,12 +58,31 @@ public class HomeController {
             model.addAttribute("title", "Add Job");
             return "add";
         }
+        Optional<Employer> newEmployer = employerRepository.findById(employerId);
+        Employer employer = (Employer) newEmployer.get();
+        newJob.setEmployer(employer);
+        List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+        newJob.setSkills(skillObjs);
 
+////*************alternate way
+//        Employer newEmployer = employerRepository.findById(employerId)
+//                .orElse(new Employer());
+//        Employer employer = (Employer) newEmployer.get();
+//        newJob.setEmployer(employer);
+//        List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+//        newJob.setSkills(skillObjs);
+
+        jobRepository.save(newJob);
         return "redirect:";
     }
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
+
+        model.addAttribute("title", "Job Listing");
+        Optional<Job>jobA= jobRepository.findById(jobId);
+        Job newJob = jobA.get(); //get info off id
+        model.addAttribute("job", newJob);
 
         return "view";
     }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,35 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    private String description;
+
+    @ManyToMany(mappedBy="skills")
+    private final List<Job> jobs= new ArrayList<>();
+
+    public Skill(){}
+
+    public Skill(String description){
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
 
 }
